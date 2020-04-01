d88f4f3c939ea95e330b0911c5bf6610b6a20b6f
Angelica <Angelica137@users.noreply.github.com>
2020-04-01 19:55:55 +0100

    add no bike avail axception

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,12 +1,22 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
-  def release_bike
-    Bike.new
+  
+  attr_reader :bike, :bike_stands
+  def initialize
+    @bike_stands = []
   end
+  def release_bike
+    if bike_stands == []
+      raise "There are no bikes here"
+    else
+      bike
+    end
+  end
+  
   def dock_bike(bike)
     @bike = bike
+    bike_stands << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,22 +5,30 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   
   it 'releases a bike from the docking station' do
-    bike = subject.release_bike
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.release_bike).to eq(bike)
+  end
+  
+  it 'confirms if the bike is working' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    subject.release_bike
     expect(bike).to be_working
   end
+  
 
   it { is_expected.to respond_to :dock_bike }
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
   it 'accepts a bike from a user and stores it' do
     bike = Bike.new
-    expect(subject.dock_bike(bike)).to eq(bike)
+    expect(subject.dock_bike(bike)).to eq([bike])
   end
   
   it 'tells the user if there is a bike in the docking station' do
-    bike = Bike.new
-    subject.dock_bike(bike)
-    expect(subject.bike).to eq bike
+    bike_stands = []
+    expect{ subject.release_bike }. to raise_error "There are no bikes here"
   end
 
 end
