e4c9be267132a5650470a26ee71e9faaf3d36fa4
Angelica <Angelica137@users.noreply.github.com>
2020-04-02 18:01:27 +0100

    refactor release_bike, dock_bike, full?, empty?

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -10,38 +10,24 @@ class DockingStation
   end
   
   def release_bike
-    if empty?
-      raise "There are no bikes here"
-    else
+      raise "There are no bikes here" if empty?
       bike
-    end
   end
   
   def dock_bike(bike)
-    if full?
-      raise "Docking station full"
-    else
+    raise "Docking station full" if full?
       @bike = bike
       bike_stands << bike
-    end
   end
 
   private
 
   def full?
-    if bike_stands.length >= capacity
-      true
-    else
-      false
-    end
+    bike_stands.length >= capacity
   end
 
   def empty?
-    if bike_stands == []
-      true
-    else
-      false
-    end
+    bike_stands == []
   end
 
 end
